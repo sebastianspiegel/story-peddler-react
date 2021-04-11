029903d8ec0a76d76a7f789b64681695496d37b6
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-04-11 13:57:22 -0400

    add functions to app to handle login and logout as well as updating login component to handle onchange and onsubmit for form

--- src/App.js
+++ src/App.js
@@ -13,13 +13,45 @@ export default class App extends React.Component{
     user: {}
   }
 
+  componentDidMount() {
+    this.loginStatus()
+  }
+
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {}
+    })
+  }
+
+  loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', 
+    {withCredentials: true})    
+    .then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
+
   render(){
     return(
       <div>
         <BrowserRouter>
           <Header />
           <Switch>
-            
+            <Route exact path='/login' component={} />
+            <Route exact path='/signup' component={} />
           </Switch>
         </BrowserRouter>
       </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,40 @@
 import React from "react";
 
 
-export default function LoginForm(){
-    <form>
-    Name: <input type="text"/>
-    Password: <input type="password" />
-    <input type="submit" />
-</form>
+// export default function LoginForm(){
+//     <form>
+//     Name: <input type="text"/>
+//     Password: <input type="password" />
+//     <input type="submit" />
+// </form>
+// }
+
+export default class Login extends React.Component{
+    state = {
+        username: '',
+        password: '',
+        errors: ''
+    }
+
+    handleChange = (e) => {
+        const {name, value} = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    render(){
+        <div>
+            <form onSubmit={this.handleSubmit}>
+            Username: <input type="text" value={this.state.username} onChange={this.handleChange} />
+            Password: <input type="password" value={this.state.password} onChange={this.handleChange} />
+            <input type="submit" value="Log In" />
+            or <Link to='/signup'>Sign Up</Link>
+            </form>
+        </div>
+    }
 }
