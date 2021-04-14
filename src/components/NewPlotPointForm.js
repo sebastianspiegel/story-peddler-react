859b51bf38cb63d47fe25354270b392133e72e13
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-04-14 15:25:02 -0400

    add new plot point form

--- src/components/NewPlotPointForm.js
+++ src/components/NewPlotPointForm.js
@@ -1,13 +1,12 @@
-export default function NewCharacter(props) {
+export default function NewPlotPoint(props) {
     return(
         <div>
             <form onSubmit={props.handleSubmit}>
-                Name: <input type="text" name="name" onChange={props.handleChange} /><br/>
                 Description: <textarea name="desc" onChange={props.handleChange} rows="2"/><br/>
-                Story: <select class="custom-select" name="story">
+                Story: <select class="custom-select" name="story" onChange={props.handleChange}>
                     <option selected="">Select a story</option>
                 </select><br/>
-                <input type="submit" value="Create Character" />
+                <input type="submit" value="Create Plot Point" />
             </form>
         </div>
     )
