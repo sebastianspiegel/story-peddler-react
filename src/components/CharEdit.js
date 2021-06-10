1ee8089f8bdc189a39225c18a337c061c5b5853f
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-06-10 15:54:38 -0400

    change charedit to functional component and fuck around with buttons some more

--- src/components/CharEdit.js
+++ src/components/CharEdit.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-export default class CharEdit extends React.Component{
+export default function CharEdit({story}){
 
-    render(){
-        return(
-            <div>
-                Editing characters 
-            </div>
+    function characters(){
+        return story.characters.map( c => 
+            <li key={c.id} className="list-group-item flex-column align-items-start" >
+                <div className="d-flex w-100 justify-content-between">
+                    {c.name}
+                </div>
+                <p className="mb-1">{c.description}</p>
+                <button className="badge bg-success">Edit</button>
+                <button className="badge bg-danger">Delete</button>
+            </li>
         )
     }
+
+   
+    return(
+        <div>
+            {characters()}
+            <button className="btn btn-outline-info" onClick={() => console.log("back")}>Return to Story</button>
+        </div>
+    )
 }
