a96a20d32a25dc770d13cd784458bcc43f9902d1
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-06-14 10:34:22 -0400

    update plot point edit to functional component

--- src/components/PlotEdit.js
+++ src/components/PlotEdit.js
@@ -1,12 +1,23 @@
 import React from "react";
+import {useHistory} from "react-router-dom"
 
-export default class PlotEdit extends React.Component{
+export default function PlotEdit({story}){
 
-    render(){
-        return(
-            <div>
-                Editing plot points  
-            </div>
+    function plotPoints(){
+        return story.plot_points.map( p =>
+            <li key={p.id} className="list-group-item list-group-item-action">{p.description}
+            <button className="badge bg-success">Edit</button>
+            <button className="badge bg-danger">Delete</button>
+            </li>
         )
     }
+
+    let history = useHistory();
+    
+    return(
+        <div>
+            {plotPoints()}
+            <button className="btn btn-outline-info" onClick={() => history.push(`/stories/${story.id}`)}>Return to Story</button>
+        </div>
+    )
 }
